2eb2d8d4be05450d9ffdc70f76bbe72be703c7ed
Tyler Austen <tausten@tunein.com>
2020-09-28 23:52:46 +0000

    First working service - also had to add 8080 to the devcontainer.json pass-through ports

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,17 +6,26 @@ package graph
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
 	"github.com/tausten/tyler-dd2020q3-todos/graph/generated"
 	"github.com/tausten/tyler-dd2020q3-todos/graph/model"
 )
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   fmt.Sprintf("T%d", rand.Int()),
+		User: &model.User{
+			ID: input.UserID, Name: "user " + input.UserID,
+		},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.todos, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
